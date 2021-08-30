f095d0ef3db40dbae67227c3e5b8b726e71554d3
LazerBeam <89406422+tanatron3939@users.noreply.github.com>
2021-08-30 15:37:05 +0700

    Add files via upload

---
+++ 23-java-script.js
@@ -0,0 +1,13 @@
+  let sid
+  let sname 
+
+  function display_data() {
+    sid = document.getElementById('sid').value
+    sname = document.getElementById('sname').value
+
+    document.getElementById('output').innerHTML= sid + ' ' + sname
+
+    document.getElementById('sid').value = ''
+    document.getElementById('sname').value = ''
+  }
+
